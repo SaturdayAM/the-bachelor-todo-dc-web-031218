15266eb32f883124cf1e265b6520a1712d7598ae
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 21:53:48 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,39 @@
+require 'pry'
+require 'json'
+#require_relative'../spec/fixtures/contestants.json'
+data = JSON.parse(File.read(File.expand_path("../../spec/fixtures/contestants.json", __FILE__)))
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  to_return = ""
+  data.each do |season_no, arr|
+    if season_no.to_s == season
+      arr.each do |innerhash|
+        #binding.pry
+        if innerhash["status"] == "Winner"
+          to_return = innerhash["name"].split.first
+        end #end if
+      end#end each
+    end#end if
+  end#end each
+  to_return
 end
+
+puts get_first_name_of_season_winner(data, "season 19")
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season_no, arr|
+    if season_no.to_s == season
+      arr.each do |innerhash|
+        #binding.pry
+        if innerhash["status"] == "Winner"
+          to_return = innerhash["name"].split.first
+        end #end if
+      end#end each
+    end#end if
+  end#end each
+  to_return
 end
 
 def count_contestants_by_hometown(data, hometown)
